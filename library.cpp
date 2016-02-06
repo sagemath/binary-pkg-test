1d5550a08df19b5cb3080c2ef57876c8f36d1697
Volker Braun <vbraun.name@gmail.com>
2016-02-07 00:16:58 +0100

    Make sure that paths are static strings in library

--- library.cpp
+++ library.cpp
@@ -4,29 +4,42 @@
 
 
 // Examples of hardcoded strings that are sometimes found in binaries
+namespace {
+  // Filename as null-terminated string
+  const static std::string lib_root = ROOT_PATH;
+  
+  // Entry in a search path with IFS
+  const static std::string lib_root_with_separator = "/foo:" ROOT_PATH ":/bar";
+  
+  // Install prefix
+  const static std::string lib_prefix = PREFIX_PATH;
+  
+  // Entry in a search path with IFS
+  const static std::string lib_prefix_with_separator = "/foo:" PREFIX_PATH ":/bar";
+}
 
 
 // Root path as null-terminated string
 std::string get_path_root() {
-  return std::string(ROOT_PATH);
+  return lib_root;
 }
 
 
 // Entry in a search path with IFS
 std::string get_path_root_with_separator() {
-  return std::string("/foo:" ROOT_PATH ":/bar");
+  return lib_root_with_separator;
 }
 
 
 // Prefix path as null-terminated string
 std::string get_path_prefix() {
-  return std::string(PREFIX_PATH);
+  return lib_prefix;
 }
 
 
 // Prefix in a search path with IFS
 std::string get_path_prefix_with_separator() {
-  return std::string("/foo:" PREFIX_PATH ":/bar");
+  return lib_prefix_with_separator;
 }
 
 
